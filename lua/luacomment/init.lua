8f9ce0c75c8e20f368c57457255e26bf14baaea3
flolap <f@linguenheld.fr>
2022-08-11 15:18:50 +0200

    Add Remove Invert

--- lua/luacomment/init.lua
+++ lua/luacomment/init.lua
@@ -1,27 +1,90 @@
 local api = vim.api
 
+local characters = {lua={"--", "-- [[", "]] --"},
+                    py={"#", '"""', '"""'},}
 
-function p()
-    print('Mange ton chien')
-
-    local file_extension = string.match(api.nvim_buf_get_name(0), "^.+(%..+)$")
-
-    local current_line = api.nvim_win_get_cursor(0)[1] - 1
-    local starting_text = api.nvim_buf_get_text(0, current_line, 0, current_line, 2, {})
-    P(starting_text)
+local infos = {file_extension="",
+               exist=false,
+               current_line=0,
+               starting_text=""}
 
 
-    print(api.nvim_buf_get_name(0))
+local function get_infos()
 
-    --vim.api.nvim_buf_set_text(0, 0, 0, 0, 0, {"#"})
+    -- TODO : Simple or block ?
+    local mode = 1
+
+    infos.file_extension = string.match(api.nvim_buf_get_name(0), "^.+%.(.+)$")
+    
+    if characters[infos.file_extension] then
+        infos.exist = true
+
+        local length = #characters[infos.file_extension][mode]
+        infos.current_line = api.nvim_win_get_cursor(0)[1] - 1
+
+        infos.starting_text = api.nvim_buf_get_text(0, infos.current_line, 0, infos.current_line, length, {})
+
+    else
+        print(infos.file_extension .. " n'existe pas :(")
+    end
+end
+
+function invert()
+
+    get_infos()
+
+    if infos.exist == true then
+
+        local text = api.nvim_get_current_line()
+
+        if not _remove(text) then
+            _add(text)
+        end
+    end
+end
+
+function _remove(text)
+
+    if #text > 0 then
+
+        local start, stop = string.find(text, characters[infos.file_extension][1], 0, true)
+
+        if start then
+            if text:sub(stop+1, stop+1) == " " then
+                stop = stop + 1
+            end
+
+            vim.api.nvim_buf_set_text(0, infos.current_line, start-1, infos.current_line, stop, {})
+
+            return true
+        end
+    end
+end
+
+function _add(text)
+
+    if #text > 0 then
+
+        -- Find the first character (! lua is base 1 and vim base 0)
+        local _, first = string.find(text, "^%s+%g")
+        if not first then
+            first = 1
+        end
+
+        vim.api.nvim_buf_set_text(0,
+                                  infos.current_line,
+                                  first-1,
+                                  infos.current_line,
+                                  first-1,
+                                  {characters[infos.file_extension][1] .. " "})
+    end
 end
 
 
--- TODO : Function to add at the beginning
--- TODO : Function to invert !
 -- TODO : Table with simple and with complex comment
 -- TODO : Command to change pattern (simple -> complex)
 -- TODO : add with O or A
 
 
-vim.api.nvim_set_keymap('n', '<leader>a', ':lua p() <cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>aa', ':lua add() <cr>', {})
+vim.api.nvim_set_keymap('n', '<leader>ar', ':lua invert() <cr>', {})
